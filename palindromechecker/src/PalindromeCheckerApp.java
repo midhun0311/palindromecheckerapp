7f1eff33e35c23522b62d2ec5a1aad111e3e3be2
midhun0311 <mp9069@srmist.edu.in>
2026-03-02 15:39:54 +0530

    UC5: Stack-Based Palindrome Checker

--- palindromechecker/src/PalindromeCheckerApp.java
+++ palindromechecker/src/PalindromeCheckerApp.java
@@ -1,32 +1,28 @@
 import java.util.Scanner;
-
+import java.util.Stack;
 public class PalindromeCheckerApp {
      public static void main(String[] args) {
-         String input = "radar";
+         String input = "noon";
 
-         // Convert the string into a character array
-         char[] chars = input.toCharArray();
+         // Create a Stack to store characters
+         Stack<Character> stack = new Stack<>();
 
-         // Initialize pointer at the beginning
-         int start = 0;
+         // Push each character of the string into the stack
+         for (char c : input.toCharArray()) {
+             stack.push(c);
+         }
 
-         // Initialize pointer at the end
-         int end = chars.length - 1;
-
-         // Assume palindrome initially
+         // Check if palindrome
          boolean isPalindrome = true;
 
-         // Continue comparison until pointers cross
-         while (start < end) {
-             if (chars[start] != chars[end]) {
+         for (char c : input.toCharArray()) {
+             if (c != stack.pop()) {
                  isPalindrome = false;
                  break;
              }
-             start++;
-             end--;
          }
 
-         // Print result
+         // Display result
          System.out.println("Input : " + input);
          System.out.println("Is Palindrome? : " + isPalindrome);
     }
